aa84a1d61cdecf61ef6b23eb4531bef1eaf8428f
Teresa <teresa.wu@ostmodern>
2018-09-12 09:53:27 +0100

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:sample/RandomWordsState.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup name generator',
+      home: RandomWords(),
+    );
+  }
+}
