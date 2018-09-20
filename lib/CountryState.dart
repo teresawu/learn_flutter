952ed388e9a3acf62820bbed68ea3f3e3e921fcd
Teresa <teresa.wu@ostmodern>
2018-09-20 23:16:24 +0100

    add listview style and icon

--- lib/CountryState.dart
+++ lib/CountryState.dart
@@ -1,43 +1,45 @@
 import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 class CountryState extends State<CountryApp> {
-  List data;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Country List"),
-        ),
-        body: new Container(
-          child: new Center(
-            // Use future builder and DefaultAssetBundle to load the local JSON file
-            child: new FutureBuilder(
-                future: DefaultAssetBundle
-                    .of(context)
-                    .loadString('data_repo/country.json'),
-                builder: (context, snapshot) {
-                  // Decode the JSON
-                  var new_data = JSON.decode(snapshot.data.toString());
-                  return new ListView.builder(
-                    // Build the ListView
-                    itemBuilder: (BuildContext context, int index) {
-                      return new Card(
-                        child: new Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: <Widget>[
-                            new Text("Name: " + new_data[index]['name']),
-                            new Text("Code: " + new_data[index]['code'])
-                          ],
+      appBar: AppBar(
+        title: Text("Country List"),
+      ),
+      body: _buildContainer(),
+    );
+  }
+
+  Widget _buildContainer() {
+    return new Container(
+      child: new Center(
+        // Use future builder and DefaultAssetBundle to load the local JSON file
+        child: new FutureBuilder(
+            future: DefaultAssetBundle
+                .of(context)
+                .loadString('data_repo/country.json'),
+            builder: (context, snapshot) {
+              // Decode the JSON
+              var new_data = JSON.decode(snapshot.data.toString());
+              return new ListView.builder(
+                  padding: const EdgeInsets.all(14.0),
+                  itemBuilder: (context, index) {
+                    if (index.isOdd) return Divider();
+                    return new ListTile(
+                        title: Text(
+                          new_data[index]['name'] +
+                              " - " +
+                              new_data[index]['code'],
+                          style: const TextStyle(fontSize: 16.0),
                         ),
-                      );
-                    },
-                    itemCount: new_data == null ? 0 : new_data.length,
-                  );
-                }),
-          ),
-        ));
+                        trailing: new Icon(Icons.directions_bike));
+                  });
+            }),
+      ),
+    );
   }
 }
 
--- lib/main.dart
+++ lib/main.dart
@@ -12,3 +12,6 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+
