dae7b0ccaa8faf171db85c10611397071113e8ed
Teresa <teresa.wu@ostmodern>
2018-11-12 22:57:18 +0000

    add websocket to send data

---
+++ lib/HomePage.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:web_socket_channel/io.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+class HomePage extends StatefulWidget {
+  final String title;
+  final WebSocketChannel channel;
+
+  HomePage({Key key, @required this.title, @required this.channel}):super(key: key);
+
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  TextEditingController controller = TextEditingController();
+  final List<String> list = [];
+
+  @override
+  void initState() {
+    super.initState();
+    widget.channel.stream.listen((data) => setState(() => list.add(data)));
+  }
+
+  void sendData() {
+    if (controller.text.isNotEmpty) {
+      widget.channel.sink.add(controller.text);
+      controller.text = "";
+    }
+  }
+
+  @override
+  void dispose() {
+    widget.channel.sink.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            Form(
+              child: TextFormField(
+                controller: controller,
+                decoration: InputDecoration(
+                  labelText: "Send to WebSocket",
+                ),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: list.map((data) => Text(data)).toList(),
+            ),
+
+//             StreamBuilder(
+//               stream: widget.channel.stream,
+//               builder: (context, snapshot) {
+//                 return Padding(
+//                   padding: const EdgeInsets.symmetric(vertical: 24.0),
+//                   child: Text(snapshot.hasData ? '${snapshot.data}' : ''),
+//                 );
+//               },
+//             ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.send),
+        onPressed: sendData,
+        tooltip: 'Send message',
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sample/CountryState.dart';
+import 'package:sample/HomePage.dart';
+import 'package:web_socket_channel/io.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,11 +8,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup',
-      home: CountryApp(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(
+        title: 'Websocket App',
+        channel: IOWebSocketChannel.connect('ws://echo.websocket.org'),
+      ),
     );
   }
 }
-
-
-
